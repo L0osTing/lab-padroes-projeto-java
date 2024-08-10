8cc8c13391f7ceaac6905f9ea66c5e073dab56c8
L0osTing <alineoliveiraa02@hotmail.com>
2024-08-10 19:46:50 -0300

    Update Facade.java
    Injeção de Dependências

--- src/one/digitalinnovation/gof/facade/Facade.java
+++ src/one/digitalinnovation/gof/facade/Facade.java
@@ -5,10 +5,19 @@ import subsistema2.cep.CepApi;
 
 public class Facade {
 
-	public void migrarCliente(String nome, String cep) {
-		String cidade = CepApi.getInstancia().recuperarCidade(cep);
-		String estado = CepApi.getInstancia().recuperarEstado(cep);
-		
-		CrmService.gravarCliente(nome, cep, cidade, estado);
-	}
+    private final CrmService crmService;
+    private final CepApi cepApi;
+
+    // Injeção de dependências via construtor
+    public Facade(CrmService crmService, CepApi cepApi) {
+        this.crmService = crmService;
+        this.cepApi = cepApi;
+    }
+
+    public void migrarCliente(String nome, String cep) {
+        String cidade = cepApi.recuperarCidade(cep);
+        String estado = cepApi.recuperarEstado(cep);
+        
+        crmService.gravarCliente(nome, cep, cidade, estado);
+    }
 }
